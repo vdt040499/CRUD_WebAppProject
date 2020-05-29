c857723b729d7c43874ac20e9d93aa1b2faef208
VoTan <vdt040499@gmail.com>
2020-05-29 08:51:51 +0700

    Update exercise 3

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -58,5 +58,337 @@ module.exports.edit = async(req, res) => {
 }
 
 module.exports.editPost = async(req, res) => {
-    res.render('')
+    const product = await  Product.findById(req.params.productId);
+        if (product.name === req.body.name) {
+            if (product.price === req.body.price) {
+                if (product.brand === req.body.brand) {
+                    if (product.amount === req.body.amount) {
+                        if (product.descript === req.body.descript) {
+                            return res.status(200).json({
+                                message: 'You changed nothing',
+                                product: product
+                            });
+                        } else {
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed descript successfully',
+                                product: product
+                            });
+                        }
+                    } else {
+                        if (product.descript === req.body.descript) {
+                            product.amount = req.body.amount;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed amount successfully',
+                                product: product
+                            });
+                        } else {
+                            product.amount = req.body.amount;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed amount and descript successfully',
+                                product: product
+                            });
+                        }
+                    }
+                } else {
+                    if (product.amount === req.body.amount) {
+                        if (product.descript === req.body.descript) {
+                            product.brand = req.body.brand;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed brand successfully',
+                                product: product
+                            });
+                        } else {
+                            product.brand = req.body.brand;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed brand and descript successfully',
+                                product: product
+                            });
+                        }
+                    } else {
+                        if (product.descript === req.body.descript) {
+                            product.brand = req.body.brand;
+                            product.amount = req.body.amount;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed brand and amount successfully',
+                                product: product
+                            });
+                        } else {
+                            product.brand  = req.body.brand;
+                            product.amount = req.body.amount;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed brand, amount and descript successfully',
+                                product: product
+                            });
+                        }
+                    }
+                }
+            } else {
+                if (product.brand === req.body.brand) {
+                    if (product.amount === req.body.amount) {
+                        if (product.descript === req.body.descript) {
+                            product.price = req.body.price;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed price successfully',
+                                product: product
+                            });
+                        } else {
+                            product.price = req.body.price;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed price and descript successfully',
+                                product: product
+                            });
+                        }
+                    } else {
+                        if (product.descript === req.body.descript) {
+                            product.price = req.body.price;
+                            product.amount = req.body.amount;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed price and amount successfully',
+                                product: product
+                            });
+                        } else {
+                            product.price = req.body.price;
+                            product.amount = req.body.amount;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed price, amount and descript successfully',
+                                product: product
+                            });
+                        }
+                    }
+                } else {
+                    if (product.amount === req.body.amount) {
+                        if (product.descript === req.body.descript) {
+                            product.price = req.body.price;
+                            product.brand = req.body.brand;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed price and brand successfully',
+                                product: product
+                            });
+                        } else {
+                            product.price = req.body.price;
+                            product.brand = req.body.brand;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed price, brand and descript successfully',
+                                product: product
+                            });
+                        }
+                    } else {
+                        if (product.descript === req.body.descript) {
+                            product.price = req.body.price;
+                            product.brand = req.body.brand;
+                            product.amount = req.body.amount;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed price, brand and amount successfully',
+                                product: product
+                            });
+                        } else {
+                            product.price  = req.body.price;
+                            product.brand = req.body.brand;
+                            product.amount = req.body.amount;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed price, brand, amount and descript successfully',
+                                product: product
+                            });
+                        }
+                    }
+                }
+            }
+        } else {
+            if (product.price === req.body.price) {
+                if (product.brand === req.body.brand) {
+                    if (product.amount === req.body.amount) {
+                        if (product.descript === req.body.descript) {
+                            product.name = req.body.name;
+                            product.save;
+                            return res.status(200).json({
+                                message: 'You changed name successfully',
+                                product: product
+                            });
+                        } else {
+                            product.name = req.body.name;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name and descript successfully',
+                                product: product
+                            });
+                        }
+                    } else {
+                        if (product.descript === req.body.descript) {
+                            product.name = req.body.name;
+                            product.amount = req.body.amount;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name and amount successfully',
+                                product: product
+                            });
+                        } else {
+                            product.name = req.body.name;
+                            product.amount = req.body.amount;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name, amount and descript successfully',
+                                product: product
+                            });
+                        }
+                    }
+                } else {
+                    if (product.amount === req.body.amount) {
+                        if (product.descript === req.body.descript) {
+                            product.name = req.body.name;
+                            product.brand = req.body.brand;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name and brand successfully',
+                                product: product
+                            });
+                        } else {
+                            product.name = req.body.name;
+                            product.brand = req.body.brand;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name, brand and descript successfully',
+                                product: product
+                            });
+                        }
+                    } else {
+                        if (product.descript === req.body.descript) {
+                            product.name = req.body.name;
+                            product.brand = req.body.brand;
+                            product.amount = req.body.amount;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name, brand and amount successfully',
+                                product: product
+                            });
+                        } else {
+                            product.name = req.body.name;
+                            product.brand = req.body.brand;
+                            product.amount = req.body.amount;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name, brand, amount and descript successfully',
+                                product: product
+                            });
+                        }
+                    }
+                }
+            } else {
+                if (product.brand === req.body.brand) {
+                    if (product.amount === req.body.amount) {
+                        if (product.descript === req.body.descript) {
+                            product.name = req.body.name;
+                            product.price = req.body.price;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name and price successfully',
+                                product: product
+                            });
+                        } else {
+                            product.name =req.body.name;
+                            product.price = req.body.price;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name, price and descript successfully',
+                                product: product
+                            });
+                        }
+                    } else {
+                        if (product.descript === req.body.descript) {
+                            product.name = req.body.name;
+                            product.price = req.body.price;
+                            product.amount = req.body.amount;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name, price and amount successfully',
+                                product: product
+                            });
+                        } else {
+                            product.name = req.body.name;
+                            product.price = req.body.price;
+                            product.amount = req.body.amount;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name, price, amount and descript successfully',
+                                product: product
+                            });
+                        }
+                    }
+                } else {
+                    if (product.amount === req.body.amount) {
+                        if (product.descript === req.body.descript) {
+                            product.name = req.body.name;
+                            product.price = req.body.price;
+                            product.brand = req.body.brand;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name, price and brand successfully',
+                                product: product
+                            });
+                        } else {
+                            product.name = req.body.name;
+                            product.price = req.body.price;
+                            product.brand = req.body.brand;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name, price, brand and descript successfully',
+                                product: product
+                            });
+                        }
+                    } else {
+                        if (product.descript === req.body.descript) {
+                            product.name =  req.body.name;
+                            product.price = req.body.price;
+                            product.brand = req.body.brand;
+                            product.amount = req.body.amount;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed name, price, brand and amount successfully',
+                                product: product
+                            });
+                        } else {
+                            product.name = req.body.name;
+                            product.price = req.body.price;
+                            product.brand = req.body.brand;
+                            product.amount = req.body.amount;
+                            product.descript = req.body.descript;
+                            product.save();
+                            return res.status(200).json({
+                                message: 'You changed price, brand, amount and descript successfully',
+                                product: product
+                            });
+                        }
+                    }
+                }
+            }
+        }
 }
